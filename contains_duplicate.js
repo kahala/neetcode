a1a7fec1b87f184a079b7ef7e2b450454de457af
L <lukedee@gmail.com>
2025-01-16 00:04:33 -0800

    added example

--- contains_duplicate.js
+++ contains_duplicate.js
@@ -1,3 +1,9 @@
-//Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Link: https://neetcode.io/problems/duplicate-integer
+
 
 console.log("hello")    
+
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
